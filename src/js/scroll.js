37e32e71cf7abec5c2df100d259c10e9ae023576
romanitwork <workromanmail@gmail.com>
2020-01-16 09:17:53 +0200

    feautures

---
+++ src/js/scroll.js
@@ -0,0 +1,12 @@
+function smoothScroll(Element) {
+
+    Element = document.getElementById(Element);
+    var selectedPosX = 0;
+    var selectedPosY = 0;
+    while (Element != null) {
+        selectedPosX += Element.offsetLeft;
+        selectedPosY += Element.offsetTop;
+        Element = Element.offsetParent;
+    }
+    window.scrollTo(selectedPosX, selectedPosY);
+    }
